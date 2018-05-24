c74b0372e54ef3add500212a9e7bc497d74dd4a3
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-24 04:33:28 +0000

    automatically backed up by learn

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,51 @@
 import React from 'react';
-import Tweet from './Tweet';
+import TweetWall from './TweetWall';
 
-class TweetWall extends React.Component {
-  constructor(props) {
-    super(props);
+import { getTweets }from '../lib/mockAPI';
+
+class App extends React.Component {
+
+  constructor() {
+    super();
 
     this.state = {
-      tweets: []
+      latestTweets: []
     };
   }
 
+  // TODO: componentWillMount()
   componentWillMount(){
-    this.setState({
-      tweets: this.props.newTweets
-    })
+    this.fetchTweets()
   }
-  
-  // TODO: shouldComponentUpdate(nextProps, nextState)
-  shouldComponentUpdate(nextProps, nextState){
-    return this.state.tweets.length > 0
+  // TODO: componentDidMount()
+  componentDidMount(){
+    this.startInterval()
   }
-  // TODO: componentWillReceiveProps()
-  componentWillReceiveProps(nextProps){
+  // TODO: componentWillUnmount()
+  componentWillUnmount(){
+    this.cleanUpInterval()
+  }
+
+  startInterval = () => {
+    this.interval = setInterval(this.fetchTweets, 2000);
+  }
+
+  cleanUpInterval = () => clearInterval(this.interval);
+
+  fetchTweets = () => {
+    const newTweets = getTweets();
     this.setState({
-      tweets: [nextProps.newTweets + this.state.tweets]
-    })
+      latestTweets: newTweets
+    });
   }
 
   render() {
-    const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
-
     return (
-      <div>{tweets}</div>
-    );
+      <div>
+        <TweetWall newTweets={this.state.latestTweets} />
+      </div>
+    )
   }
 }
 
-export default TweetWall;
+export default App;
